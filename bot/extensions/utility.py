f2fc8017f363bb8ac3afddabd68024982b6464c2
09ghostrider <57857850+007-NIKSH@users.noreply.github.com>
2022-04-30 17:15:11 +0530

    disabled banner due to API issues

--- bot/extensions/utility.py
+++ bot/extensions/utility.py
@@ -23,21 +23,21 @@ async def _av(ctx: lightbulb.Context) -> None:
     embed.set_image(member.display_avatar_url)
     await ctx.respond(embed=embed, reply=True)
 
-@plugin.command()
-@lightbulb.option("member", "the member to show", type=hikari.Member, required=False, default=None)
-@lightbulb.command("banner", "shows the members display banner")
-@lightbulb.implements(lightbulb.PrefixCommand)
-async def _banner(ctx: lightbulb.Context) -> None:
-    member = ctx.options.member
-    if member == None:
-        member = ctx.event.message.member
+# @plugin.command()
+# @lightbulb.option("member", "the member to show", type=hikari.Member, required=False, default=None)
+# @lightbulb.command("banner", "shows the members display banner")
+# @lightbulb.implements(lightbulb.PrefixCommand)
+# async def _banner(ctx: lightbulb.Context) -> None:
+#     member = ctx.options.member
+#     if not member:
+#         member = ctx.event.message.member
     
-    embed = hikari.Embed(color=random.randint(0x0, 0xffffff), title=f"{member}'s banner")
-    if member.banner_url != None:
-        embed.set_image(member.banner_url)
-    else:
-        embed.description("No banner set")
-    await ctx.respond(embed=embed, reply=True)
+#     embed = hikari.Embed(color=random.randint(0x0, 0xffffff), title=f"{member}'s banner")
+#     if member.banner_url != None:
+#         embed.set_image(member.banner_url)
+#     else:
+#         embed.description = "No banner set"
+#     await ctx.respond(embed=embed, reply=True)
 
 def load(bot):
     bot.add_plugin(plugin)
